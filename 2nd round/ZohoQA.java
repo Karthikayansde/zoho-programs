1f0ac0e7e789227186bb75793c636d152bb212d4
R. karthi kayan <102909171+Karthikayansde@users.noreply.github.com>
2022-11-09 22:53:19 +0530

    Add files via upload

--- 2nd round/ZohoQA.java
+++ 2nd round/ZohoQA.java
@@ -1,28 +1,44 @@
+import java.util.ArrayList;
+// input = {1,2,3,4,5,6,7,8,9}
+// output = {1,8,3,6,5,4,7,2,9}
+
 public class ZohoQA {
-    // 4 2 3 6 = 12
-    // 3 6 4 8 5 5 = 26
     public static void main(String[] args) {
-        int a[] = {4,2,3,6};
-        int b[] = {3,6,4,8,5,5};
-        int back = 0,current = 0,sum = 0;
-        for(int i=0;i<b.length;i++)
-        {
-            if(i==0)
-            {
-                back = b[i];
-                sum = back;
+        int ar[] = {1,2,3,4,5,6,7,8,9};
+        ArrayList<Integer> ar1 = new ArrayList<Integer>();
+        ArrayList<Integer> ar2 = new ArrayList<Integer>();
+        for (int integer : ar) {
+            if(integer % 2 == 0){
+                ar2.add(integer);
             }
             else{
-                current = b[i];
-                if(back%2==0 && current%2!=0)
-                {
-                    back = current;
-                    continue;
-                }
-                sum = sum + current;
-                back = current;
+                ar1.add(integer);
             }
         }
-        System.out.println(sum);
+        int artemp[] = new int[ar2.size()];
+        int a = 0;
+        for (int ele : ar2) {
+            artemp[a] = ele;
+            a++;
+        }
+        for (int i = 0,j = artemp.length-1; i < ar1.size()||j>=0; i++,j--) {
+            if(i<ar1.size()){
+                System.out.println(ar1.get(i));
+            }
+            if(j>=0){
+                System.out.println(artemp[j]);
+            }
+        }
+        ar1 = sort(ar1);
+    }
+    static ArrayList<Integer> sort(ArrayList<Integer> ar3)
+    {
+        int start = 0;
+        for (start = 0; start < ar3.size(); start++) {
+            for (int i = 0; i < array.length; i++) {
+                
+            }
+        }
+        return ar3;
     }
 }
